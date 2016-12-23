a9ade86d2745b4474eb21243ab0ce194613c7348
Ivan Kiliian <ivkilnau@gmail.com>
2016-12-23 14:42:00 +0200

    Set state strategy for forecast view

--- app/src/main/java/com/kiliian/sunshine/mvp/views/ForecastView.java
+++ app/src/main/java/com/kiliian/sunshine/mvp/views/ForecastView.java
@@ -2,10 +2,13 @@ package com.kiliian.sunshine.mvp.views;
 
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.kiliian.sunshine.data.Weather;
 
 import java.util.List;
 
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface ForecastView extends MvpView {
 
     void showForecast(List<Weather> weatherList);
