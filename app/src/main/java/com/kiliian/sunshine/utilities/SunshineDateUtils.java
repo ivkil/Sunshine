7b39caf8d18c4101c91ea75547fe0eb92ba66ce7
Ivan Kiliian <ivkilnau@gmail.com>
2017-01-04 07:28:31 +0200

    Supporting Ukrainian

--- app/src/main/java/com/kiliian/sunshine/utilities/SunshineDateUtils.java
+++ app/src/main/java/com/kiliian/sunshine/utilities/SunshineDateUtils.java
@@ -1,6 +1,8 @@
 package com.kiliian.sunshine.utilities;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
 import android.text.format.DateFormat;
 
 import com.kiliian.sunshine.R;
@@ -9,7 +11,10 @@ import org.threeten.bp.LocalDate;
 import org.threeten.bp.format.DateTimeFormatter;
 import org.threeten.bp.format.TextStyle;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 
 public class SunshineDateUtils {
@@ -21,10 +26,10 @@ public class SunshineDateUtils {
         LocalDate today = LocalDate.now();
         if (date.isEqual(today) || showFullDate) {
             String dayName = getDayName(context, date);
-            String readableDate = getReadableDateString(date, DATE_FORMAT_FULL);
+            String readableDate = getReadableDateString(context, date, DATE_FORMAT_FULL);
             if (!date.isAfter(today.plusDays(1))) {
-                String pattern = DateFormat.getBestDateTimePattern(Locale.getDefault(), "EEEE");
-                String localizedDayName = date.format(DateTimeFormatter.ofPattern(pattern));
+                String pattern = DateFormat.getBestDateTimePattern(getLocale(context), "EEEE");
+                String localizedDayName = date.format(DateTimeFormatter.ofPattern(pattern, getLocale(context)));
                 return readableDate.replace(localizedDayName, dayName);
             } else {
                 return readableDate;
@@ -32,13 +37,14 @@ public class SunshineDateUtils {
         } else if (!date.isAfter(today.plusWeeks(1))) {
             return getDayName(context, date);
         } else {
-            return getReadableDateString(date, DATE_FORMAT_SHORT);
+            return getReadableDateString(context, date, DATE_FORMAT_SHORT);
         }
     }
 
-    private static String getReadableDateString(LocalDate date, String format) {
-        String pattern = DateFormat.getBestDateTimePattern(Locale.getDefault(), format);
-        return date.format(DateTimeFormatter.ofPattern(pattern));
+    private static String getReadableDateString(Context context, LocalDate date, String format) {
+        Locale locale = getLocale(context);
+        String pattern = DateFormat.getBestDateTimePattern(locale, format);
+        return date.format(DateTimeFormatter.ofPattern(pattern, locale));
     }
 
     private static String getDayName(Context context, LocalDate date) {
@@ -49,6 +55,23 @@ public class SunshineDateUtils {
         if (date.isEqual(today.plusDays(1))) {
             return context.getString(R.string.tomorrow);
         }
-        return date.getDayOfWeek().getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault());
+        return date.getDayOfWeek().getDisplayName(TextStyle.FULL_STANDALONE, getLocale(context));
+    }
+
+    private static Locale getLocale(Context context) {
+        final Set<String> AVAILABLE_TRANSLATIONS = new HashSet<>(Arrays.asList(
+                new String[]{"uk"}
+        ));
+        Resources resources = context.getResources();
+        Locale locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locale = resources.getConfiguration().getLocales().get(0);
+        } else {
+            locale = resources.getConfiguration().locale;
+        }
+        if (!AVAILABLE_TRANSLATIONS.contains(locale.getLanguage())) {
+            locale = Locale.ENGLISH;
+        }
+        return locale;
     }
 }
